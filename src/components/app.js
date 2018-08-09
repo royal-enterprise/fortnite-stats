900ec2e1839f854121da95c7dc7d4de505a28bd8
cloud <junehoe@clouds-MacBook-Pro.local>
2018-08-08 18:42:08 -0700

    Adds skeleton

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+const API_KEY = 'a6defb3f-aeab-4851-a0af-a4510039ca1b';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+}
